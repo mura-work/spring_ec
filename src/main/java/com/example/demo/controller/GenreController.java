c83bcd878f28a8d70ba0aecda1325dcf94472499
murata <murata.small.baseball@gmail.com>
2021-06-01 06:20:32 +0900

    genre indexの作成

--- src/main/java/com/example/demo/controller/GenreController.java
+++ src/main/java/com/example/demo/controller/GenreController.java
@@ -1,8 +1,20 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+
+import com.example.demo.form.GenreForm;
+import com.example.demo.repository.GenreRepository;
 
 @Controller
 public class GenreController {
+	@Autowired
+	GenreRepository repository;
 	
+	@GetMapping("/genres")
+	public String index(@ModelAttribute GenreForm form) {
+		return "genres/index";
+	}
 }
