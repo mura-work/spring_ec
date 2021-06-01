4646583646bb950d0bff89216456dac093f46442
murata <murata.small.baseball@gmail.com>
2021-06-02 07:11:53 +0900

    JUnit実行されない問題修正中

--- src/test/java/com/example/demo/controller/GenreControllerTest.java
+++ src/test/java/com/example/demo/controller/GenreControllerTest.java
@@ -1,4 +1,4 @@
-package com.example.demo.genre;
+package com.example.demo.controller;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
@@ -7,17 +7,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import com.example.demo.controller.GenreController;
-
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest
 public class GenreControllerTest {
 	
 	private MockMvc mockMvc;
