841e230ab02aaca8b17da49e7f02d10ea81be5c6
murata <murata.small.baseball@gmail.com>
2021-06-07 20:37:02 +0900

    products生成

--- src/main/java/com/example/demo/controller/ProductController.java
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -1,5 +1,21 @@
 package com.example.demo.controller;
 
-public class ProductController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.demo.repository.ProductRepository;
+
+@Controller
+public class ProductController {
+	
+	@Autowired
+	ProductRepository repository;
+	
+	@GetMapping("/products")
+	public String index(Model model) {
+		model.addAttribute("products", repository.findAll());
+		return "products/index";
+	}
 }
