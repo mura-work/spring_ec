d4ceab8bc71e1b1cd3e8bc74e92d69ac77da217b
murata <murata.small.baseball@gmail.com>
2021-06-10 21:11:11 +0900

    spring security導入

--- src/main/java/com/example/demo/controller/EmployeeController.java
+++ src/main/java/com/example/demo/controller/EmployeeController.java
@@ -1,5 +1,23 @@
 package com.example.demo.controller;
 
-public class EmployeeController {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
+import com.example.demo.repository.EmployeeRepository;
+
+@Controller
+public class EmployeeController {
+	@Autowired
+	EmployeeRepository repository;
+	
+	@GetMapping("/login")
+	public String login() {
+		return "emp/login";
+	}
+	
+	@GetMapping("/logout")
+	public String logout() {
+		return "redirect:/emp/login";
+	}
 }
